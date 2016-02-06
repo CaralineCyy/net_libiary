033cf5d712f178e260540371240ffaf1528cfc86
yunyun.cai <1835373375@qq.com>
2016-02-06 16:51:26 +0800

    c#处理数据库通用方法

---
+++ AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("SZSOFT.DBUtility(神州共享)")]
+[assembly: AssemblyDescription("Data Access Application Model By SZSOFT")]
+[assembly: AssemblyConfiguration("SZSOFT")]
+[assembly: AssemblyCompany("SZSOFT")]
+[assembly: AssemblyProduct("SZSOFT.DBUtility")]
+[assembly: AssemblyCopyright("Copyright (C) SZSOFT 2014-2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("3.5.0")]
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyKeyName("")]
